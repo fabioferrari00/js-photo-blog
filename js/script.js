74c2ff4de83b6dcbf42a86fb00746e766454e8ef
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-21 18:06:12 +0200

    [fix] remove array

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 //mi recupero l'elemento del dom nella quale andrò a stampare le images
 const imagesList = document.getElementById('images-list');
 
-let cards = [];
 //creo un ciclo di 6 iterazioni per generare le cards
 for (let i = 0; i < 6; i++) {
 
   //effettuo la chiamata ajax
   axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((resp) => {
 
-
-    cards.push(resp.data[i]);
-    let card = resp.data[i];
-
-
     //inserisco nell'html per ogni iterazione una card
     imagesList.innerHTML += `<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
         <div class="card" style="width: 18rem;">
-          <img src="${card.url}" class="card-img-top p-3" alt="...">
+          <img src="${resp.data[i].url}" class="card-img-top p-3" alt="...">
           <img src="./img/pin.svg" alt="" class="pin">
           <div class="card-body">
-            <h3>${card.title}</h3>
-            <p class="card-text">${card.date}</p>
+            <h3>${resp.data[i].title}</h3>
+            <p class="card-text">${resp.data[i].date}</p>
           </div>
         </div>
       </div>`;
